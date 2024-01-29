edbc7431f61151a3408925b42a0e2424cd038f1b
lukman-hakim-123 <lh146244@gmail.com>
2024-01-29 09:40:07 +0700

    input

--- basic_dart/lib/input.dart
+++ basic_dart/lib/input.dart
@@ -1,7 +1,18 @@
 import 'dart:io';
 
 void main() {
+// input string
   print("Masukan nama: ");
   String? nama = stdin.readLineSync();
   print("nama anda adalah ${nama}");
+
+  // inpute integer
+  print("Masukkan bilangan bulat:");
+  int? nomor = int.parse(stdin.readLineSync()!);
+  print("Bilangan yang anda masukkan ${nomor}");
+
+  // float
+  print("Masukkan bilangan desimal:");
+  double number = double.parse(stdin.readLineSync()!);
+  print("Bilangan yang anda masukkan $number");
 }
