1738ef66f54e915ee281b8751a9e8b57be946b82
lukman-hakim-123 <lh146244@gmail.com>
2024-01-29 10:55:33 +0700

    switch

---
+++ basic_dart/lib/switch.dart
@@ -0,0 +1,19 @@
+void main() {
+  int posSaatIni = 10;
+  switch (posSaatIni) {
+    case 1:
+      print("Makan Snack");
+      break;
+    case 2:
+      print("Makan Snack");
+      break;
+    case 3:
+      print("Makan Snack");
+      break;
+    case 4:
+      print("Makan Snack");
+      break;
+    default:
+      print("Pos yang anda masukkan tidak terdaftar");
+  }
+}
