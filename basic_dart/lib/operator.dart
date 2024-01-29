55005de256fe2fc05d38ed05a8c0d82b378dbd08
lukman-hakim-123 <lh146244@gmail.com>
2024-01-29 09:22:38 +0700

    operator

---
+++ basic_dart/lib/operator.dart
@@ -0,0 +1,17 @@
+void main() {
+  // operator aritmatika +-*/%
+  int c = 6;
+  c = c + 6;
+
+// Operator Relasional > < >= <= != ==
+  String kota = 'Jepara';
+  if (kota == 'Jepara') ;
+
+// Operator Cek Tipe Data as is is!
+
+// Operator Logika ! || &&
+  var A = true;
+  var B = false;
+  var expr = A && B;
+  print(expr);
+}
